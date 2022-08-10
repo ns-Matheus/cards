b2623769ce286bbbd7da86e7f66886176fa47e11
ns-Matheus <snmatheus9147@gmail.com>
2022-08-10 17:49:16 -0300

    update3

--- styles/js/vue.js
+++ styles/js/vue.js
@@ -1,28 +1,85 @@
 var app = new Vue({
   el: '#app',
   data: {
+    show: false,
     lista: [],
-    valor: [],
-    carname: [],
+    valor: 0,
+    carname: 0,
     cores: [],
-    km: [],
-    aceleracao: [],
-    velocidade: [],
-    aderencia: [],
-    frenagem: []
+    kg: 0,
+    aceleracao: 0,
+    velocidade: 0,
+    aderencia: 0,
+    frenagem: 0
   }
 })
 
-function mostrarcarro() {
+
+
+function mostrarcarro(carro){
+
+  app.kg = carro.kg
+  
+  fetch("http://monkey_concessionaria/mostrarCarro", {
+    method: 'POST',
+    body: JSON.stringify({nome: carro.nome})
+  }).then(function(response) {
+        return response.json()
+  }).then(function(json) {
+   
+
+  }).catch(function () {
+        
+  })
+}
+
+function mudarcor(){
+  
+}
+
+function testedrive(){
 
 }
 
-function mudarcor() {
+function comprar(){
 
+}
+
+function sair() {
+  app.show = false
+  fetch("http://monkey_concessionaria/sair", {
+    method: 'GET' 
+
+  }).then(function(response) {
+        return response.json()
+  }).then(function(json) {
+    
+  }).catch(function () {
+    
+  })
 }
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+	window.addEventListener("message", function (event) {
+      var data = event.data
 
+      if (data.acao == true) {
+         app.show = true,
+         app.lista = data.carros
+      } 
+      
+      if (data.acao == 'att') {
+        
+      }
+   })
 
+   document.onkeyup = function(data) {
+		if (data.which == 27){
+         sair(false)
+		}
+	}
+
+})
 
